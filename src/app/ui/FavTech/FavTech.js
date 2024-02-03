973df449fd85505aed3623a0fdb145762ff0f9fa
jduncan017 <emailjoshduncan@gmail.com>
2024-02-03 01:40:43 -0700

    classname fixes

--- src/app/ui/FavTech/FavTech.js
+++ src/app/ui/FavTech/FavTech.js
@@ -8,15 +8,15 @@ import ImageLoadingWrapper from "@/src/utils/PreLoader/ImageLoadingWrapper";
 
 export const FavTech = () => {
   return (
-    <section id="techItem-section" className="techItem-outer-container">
-      <div className="techItems__header-section">
-        <div className="techItems__bottom-border">
+    <section id="fav-tech__section" className="fav-tech__outer-container">
+      <div className="fav-tech__header-section">
+        <div className="fav-tech__bottom-border">
           <h1 className="fav-tech__header">Favorite Technologies</h1>
         </div>
       </div>
       {favoriteTechData.map((techItem) => {
         return (
-          <div key={techItem.name} className="techItem-container">
+          <div key={techItem.name} className="fav-tech__container">
             <Link
               rel="noopener noreferrer"
               tabIndex="-1"
@@ -31,7 +31,7 @@ export const FavTech = () => {
                 />
               </div>
             </Link>
-            <div className="techItem-info">
+            <div className="fav-tech__info">
               <div className="techItem__text-container">
                 <div className="techItem__title-container">
                   <Image
@@ -41,11 +41,11 @@ export const FavTech = () => {
                     width={20}
                     height={20}
                   />
-                  <h3 className="techItem-title">{techItem.name}</h3>
+                  <h3 className="fav-tech__title">{techItem.name}</h3>
                 </div>
-                <p className="techItem-description">{techItem.description}</p>
+                <p className="fav-tech__description">{techItem.description}</p>
               </div>
-              <div className="techItem-buttons-container">
+              <div className="fav-tech__buttons-container">
                 <Link
                   rel="noopener noreferrer"
                   tabIndex="-1"
@@ -53,7 +53,7 @@ export const FavTech = () => {
                   href={techItem.liveLink}
                   target="_blank"
                 >
-                  <button className="techItem-button global__button">
+                  <button className="fav-tech__button global__button">
                     {techItem.liveButtonText}
                   </button>
                 </Link>
