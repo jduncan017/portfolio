a913f9225bad7a72007af9cb36fe1c3b2090aaa4
jduncan017 <emailjoshduncan@gmail.com>
2024-02-02 20:00:32 -0700

    update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,29 @@ import { cardo } from "./ui/fonts";
 import "./globals.css";
 
 export const metadata = {
-  title: "Josh Duncan's Portfolio",
-  description: "Josh Duncan's professional software engineering portfolio",
+  title: "Joshua Duncan's Software Engineering Portfolio",
+  keywords: "software, engineer, developer, web, portfolio, resume, career",
+  authors: { name: "Joshua Duncan" },
+  creator: "Joshua Duncan",
+  publisher: "Joshua Duncan",
+  description:
+    "Explore Joshua Duncan's innovative software engineering portfolio. Discover projects showcasing expertise in coding, design, and technology solutions.",
+  alternates: {
+    canonical: "https://www.joshuaduncan.info",
+  },
+  openGraph: {
+    images: {
+      url: "https://www.joshuaduncan.info/open-graph-preview.png",
+      width: 1903,
+      height: 997,
+    },
+    title: "Joshua Duncan's Software Engineering Portfolio",
+    description:
+      "Explore Joshua Duncan's innovative software engineering portfolio. Discover projects showcasing expertise in coding, design, and technology solutions.",
+    url: "https://www.joshuaduncan.info",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
